977a0214c4ee3102f09bbdd4f79880d019d979cd
Rahim Lalani <rahim@Rahims-MBP.attlocal.net>
2023-12-05 17:00:01 -0600

    Adding tabs and inital code to read historical data

--- ff-website/ff-website.py
+++ ff-website/ff-website.py
@@ -9,6 +9,7 @@ import pandas as pd
 import streamlit as st
 
 
+# Depicts the standings, power rankings, and current weekly scorebard for the current season
 def current():
     st.write('current season')
 
@@ -16,7 +17,16 @@ def current():
 # Depicts the overall statistics for players who have played in the League
 def historical():
     hist = pd.read_csv('data/historical_records.csv')
+    years_played = hist.groupby('Owner')['Year'].count().reset_index().sort_values(by=['Year', 'Owner'], ascending=[False, True]).reset_index(drop=True)
+    st.write('Years played per player')
+    st.table(years_played)
+    # for each owner, go into new function and create the data requiredto create the final chart
     st.table(hist)
+
+
+# Takes the Owner filtered historical records and creates the overall table depicting the results of that Owner
+def historical_filtering(df):
+    pass
 
 
 if __name__ == "__main__":
@@ -29,3 +39,11 @@ if __name__ == "__main__":
     
     with tab2:
         current()
+
+    
+    """
+    Notes to remember:
+    For adding images/emoji trophies in historical
+        https://discuss.streamlit.io/t/add-image-and-header-to-streamlit-dataframe-table/36065/3
+    
+    """
